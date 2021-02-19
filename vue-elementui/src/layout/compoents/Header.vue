8d0e51f4b44cbf9e586a65d0af4710eb1bdd6d1a
WangHongTing <2769077078@qq.com>
2021-02-19 17:27:06 +0800

    【功能修改】 头部修改

--- vue-elementui/src/layout/compoents/Header.vue
+++ vue-elementui/src/layout/compoents/Header.vue
@@ -1,15 +1,33 @@
 <template>
     <el-header>
-        Header,
-        <el-button type="info" @click="goBack()">退出</el-button>
+        <div>首页</div>
+        <el-dropdown>
+            <span class="el-dropdown-link">
+            退出<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item @click="goBack()">退出</el-dropdown-item>
+                <!--<el-dropdown-item disabled>双皮奶</el-dropdown-item>-->
+                <!--<el-dropdown-item divided>蚵仔煎</el-dropdown-item>-->
+            </el-dropdown-menu>
+        </el-dropdown>
     </el-header>
 </template>
 
 <script>
+    import {logout} from '@/api/login'
+    import {delToken} from "@/utils/token";
+
     export default {
-        methods:{
-            goBack(){
-                this.$router.push('/login')
+        methods: {
+            goBack() {
+                logout().then((res) => {
+                    delToken();
+                    let {code} = res.data;
+                    if (code === 20000) {
+                        this.$router.push('/login')
+                    }
+                })
             }
         },
     }
@@ -17,9 +35,16 @@
 
 <style scoped>
     .el-header {
-        background-color: #B3C0D1;
         color: #333;
-        text-align: center;
-        line-height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .el-dropdown-link {
+        cursor: pointer;
+        color: #409EFF;
+    }
+    .el-icon-arrow-down {
+        font-size: 12px;
     }
 </style>

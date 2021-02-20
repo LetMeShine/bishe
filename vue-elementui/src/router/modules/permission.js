70d6cf208aab23b4405bc69f6c996f33039ddb0b
WangHongTing <2769077078@qq.com>
2021-02-20 16:13:47 +0800

    【功能功能新增】 权限配置模块

---
+++ vue-elementui/src/router/modules/permission.js
@@ -0,0 +1,21 @@
+const permission = {
+    path: '/permission', //权限管理
+    name: 'permission',
+    meta:{title:'权限管理'},
+    component: () => import( '@/views/Home/permission/index.vue'),
+    children:[
+        {
+            path: 'create',  //创建管理员
+            name: 'create',
+            meta:{title:'创建管理员'},
+            component: () => import( '@/views/Home/permission/Create.vue')
+        },
+        {
+            path: 'list',  //列表展示
+            name: 'list',
+            meta:{title:'列表展示'},
+            component: () => import( '@/views/Home/permission/UserList.vue')
+        },
+    ]
+}
+export default  permission

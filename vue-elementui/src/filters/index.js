3ea363ff15ebe2938e753827ebfc09a62bc8dff3
WangHongTing <2769077078@qq.com>
2021-02-20 15:28:43 +0800

    【功能新增】 公共过滤器

---
+++ vue-elementui/src/filters/index.js
@@ -0,0 +1,10 @@
+// 注册全局的过滤器，也可以用这种方式注册全局的组件
+import Vue from 'vue'
+import * as filters from './filters'  //相当于取出所有，别名   filters是一个对象
+
+Object.keys(filters).forEach(key=>{
+    console.log(key);
+    console.log(filters[key]);
+    Vue.filter(key,filters[key])
+})
+

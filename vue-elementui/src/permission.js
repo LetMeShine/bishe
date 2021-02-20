43ccd382dfd12c702824b2fe53f92cc3d9340468
WangHongTing <2769077078@qq.com>
2021-02-20 16:12:35 +0800

    【功能新增】 路由拦截

---
+++ vue-elementui/src/permission.js
@@ -0,0 +1,30 @@
+import router from './router'
+import store from './store'
+// 后端传递用户信息
+// 路由拦截
+router.beforeEach(async (to, from, next) => {
+    // 1.是否是登录页
+    if (to.path === '/login') {
+        next();
+    } else {
+        // 正常登录,用户信息
+        let role = store && store.getters.roles && store.getters.roles.length > 0;
+        if (role) {
+            next();
+        } else {
+            // 获取角色，发请求 await
+            let {roles} = await store.dispatch('user');
+            //取出角色
+            let rolesName= roles.map(v=>v.name);
+            //过滤角色
+            let filterRoutes = await store.dispatch('GENERATEROUTES',rolesName);
+            //动态添加
+            router.addRoutes(filterRoutes);
+            if (roles) {
+                next({...to});// 页面刷新的话，保持在当前页面
+            } else {
+                next({path: '/login'})
+            }
+        }
+    }
+})

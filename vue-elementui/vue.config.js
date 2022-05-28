069ec845caaaf64b22f33776736f328f659f9fd1
LetMeShine <2769077078@qq.com>
2022-05-28 10:55:29 +0800

    项目访问不了问题

--- vue-elementui/vue.config.js
+++ vue-elementui/vue.config.js
@@ -3,11 +3,11 @@ let CompressionWebpackPlugin = require("compression-webpack-plugin")
 module.exports = {
     // publicPath: './',
     // 打包到子域下面  http://www.baidu.com/sub
-    publicPath: '/loanWeb/', 
+    publicPath: '/loanWeb/',
     outputDir: "dist/loanWeb",      //输出目录
-    productionSourceMap: process.env.NODE_ENV==='production' ? false : true,
+    productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
     //配置webpack
-    configureWebpack: config=>{
+    configureWebpack: config => {
         let plugins = [
             new CompressionWebpackPlugin({
                 algorithm: 'gzip', //压缩方式
@@ -18,14 +18,14 @@ module.exports = {
         if (process.env.NODE_ENV === 'production') {
             config.mode = "production";
             config.plugins = [...config.plugins, ...plugins]
-        }else{
+        } else {
             config.mode = "development"
         }
-        Object.assign(config, {
-            externals: {    //排除不需要依赖包
-                vue: 'Vue',  //import Vue from 'vue'
-                axios: 'axios' //import axios from 'axios'
-            }
-        })
+        // Object.assign(config, {
+        // externals: {    //排除不需要依赖包
+        // vue: 'Vue',  //import Vue from 'vue'
+        // axios: 'axios' //import axios from 'axios'
+        // }
+        // })
     }
 }
